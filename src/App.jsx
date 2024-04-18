47a9d53aeea1bbfc93998ee9b540afaf3c670dbe
TetiZ <tommytaba99@gmail.com>
2024-04-18 21:14:19 +0200

    pre-work settings + favicon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FaviconTheme from "./components/FaviconTheme/FaviconTheme";
+
+function App() {
+  return (
+    <>
+      <FaviconTheme></FaviconTheme>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FaviconTheme/FaviconTheme.jsx
@@ -0,0 +1,35 @@
+import { useState, useRef, useEffect } from "react";
+
+export default function FaviconTheme() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const faviconRef = useRef(null);
+
+  const lightFaviconPath = "/public/logo-light.png";
+  const darkFaviconPath = "/public/logo-dark.png";
+
+  useEffect(() => {
+    const updateFavicon = () => {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        faviconRef.current.href = darkFaviconPath;
+        setDarkTheme(true);
+      } else {
+        faviconRef.current.href = lightFaviconPath;
+        setDarkTheme(false);
+      }
+    };
+
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChangeTheme = () => {
+      updateFavicon();
+    };
+
+    isDark.addEventListener("change", handleChangeTheme);
+
+    return () => {
+      isDark.removeEventListener("change", handleChangeTheme);
+    };
+  }, []);
+
+  return null;
+}
