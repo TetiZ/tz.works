91a32de54efd9b98b86a6d133cc38066f815b93d
TetiZ <tommytaba99@gmail.com>
2024-04-25 19:58:27 +0200

    Update AboutPage.jsx

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -12,9 +12,9 @@ export default function AboutPage() {
       </h2>
       <h3 className={css.title}>My name is Tetiana</h3>
       <p className={css.about}>
-        This site was created to provide you with information about myself and
-        my projects in web development. The main goal is to show the portfolio
-        of a novice web developer.
+        This site was created to provide you information about myself and my
+        projects in web development. The main goal is to show the portfolio of a
+        novice web developer.
         <br />
         <br />
         I enjoy addressing challenges in a general sense. In my professional
